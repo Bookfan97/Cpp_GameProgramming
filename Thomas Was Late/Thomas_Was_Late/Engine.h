88ca69f0cf17a9a70e1f898ce0c25706f5747180
Noah Day <sheltielover432@gmail.com>
2020-06-14 19:22:20 -0400

    Level Manager

--- Thomas Was Late/Thomas_Was_Late/Engine.h
+++ Thomas Was Late/Thomas_Was_Late/Engine.h
@@ -3,6 +3,7 @@
 #include "TextureHolder.h"
 #include "Thomas.h"
 #include "Bob.h"
+#include "LevelManager.h"
 
 using namespace sf;
 
@@ -12,6 +13,8 @@ private:
 	TextureHolder th;
 	Thomas m_Thomas;
 	Bob m_Bob;
+	LevelManager m_LM;
+
 	const int TILE_SIZE = 50, VERTS_IN_QUAD = 4, GRAVITY = 300;
 	RenderWindow m_Window;
 	View m_MainView, m_LeftView, m_RightView, m_BGMainView, m_BGLeftView, m_BGRightView, m_HudView;
@@ -20,9 +23,13 @@ private:
 	bool m_Playing = false, m_Character1 = true, m_SplitScreen = false, m_NewLevelRequired = true;
 	float m_TimeRemaining = 10;
 	Time m_GameTimeTotal;
+	VertexArray M_VALevel;
+	int** m_ArrayLevel = NULL;
+	Texture m_TextureTiles;
 	void input();
 	void update(float dtAsSeconds);
 	void draw();
+	void loadLevel();
 public:
 	Engine();
 	void run();
